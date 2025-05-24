2a5e48e5a8973dfd1c78868f91001c36c514f965
Shivam_Khare <shivamkhare6762@gmail.com>
2025-05-24 16:15:33 +0530

    added more functional component RTE SIgnUP PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,33 @@
-import Header from "./Header/Header.jsx";
-import Fotter from "./fotter/Fotter.jsx";
-import Loading from "./Loading.jsx";
 import Container from "./container/Container.jsx";
+import Fotter from "./fotter/Fotter.jsx";
+import Header from "./Header/Header.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Postform from "./Postform/Postform.jsx";
+import Protected from "./AuthLayout.jsx";
+import CustomBtn from "./CustomBtn.jsx";
+import Input from "./Input.jsx";
+import Login from "./Login.jsx";
 import Logo from "./Logo.jsx";
+import PostCard from "./PostCard.jsx";
+import RTE from "./RTE.jsx";
+import Select from "./Select.jsx";
+import Signup from "./Signup.jsx";
 
 
 export {
-    Header,
-    Fotter,
-    Loading,
     Container,
+    Fotter,
+    Header,
     LogoutBtn,
-    Logo
+    Postform,
+    Protected,
+    CustomBtn,
+    Input,
+    Login,
+    Logo,
+    PostCard,
+    RTE,
+    Select,
+    Signup
+
 }
