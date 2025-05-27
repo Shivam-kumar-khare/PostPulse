e3ca4b3509108e6b7eb238e387c0fccc3097bc2f
Shivam_Khare <shivamkhare6762@gmail.com>
2025-05-27 17:24:20 +0530

    Project Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './services/auth.services.js';
+import { login, logout } from './redux/authSlice.js';
+import { Header, Footer, Container } from "./components/index.js";
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(!true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getAccount()
+      .then((userData) => {
+        if (userData) dispatch(login({ userData }));
+        else dispatch(logout());
+      })
+      .catch((e)=>{throw Error("User Data Cannot be fetched")})
+      .finally(() => setLoading(false))
+  }, [])
+  console.log("loading status", loading)
+  if (loading) {
+    return (
+      <>
+        <div className='w-full py-8'>
+          <Container>
+            <p className='font-bold text-center'>Loading...</p>
+          </Container>
+        </div>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+
+        <Header />
+        <Outlet></Outlet>
+        <div className='relative w-full bottom-0'><Footer /></div>
+
+      </>
+    )
+  }
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,81 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import { StrictMode } from 'react';
+import store from "./store/store.js";
+import { Provider, useSelector } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { AuthLayout, Login } from './components/index.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { AddPostPage, AllPostPage, EditPostPage, HomePage, LoginPage, Post_Page, SignupPage } from "./pages/index.js";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage  />
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage />
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPostPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPostPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPostPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post_Page />,
+      },
+    ]
+
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 )
